f0c565b56b2a1f240cb4739d0eaf84ff28ccdf46
Piyush Verma <61598060+piyushverma-coder@users.noreply.github.com>
2021-04-25 18:27:36 +0530

    boilerplate code
    the starting code without mongoose and multer

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.sendStatus(200)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
